830305c12f2b6c2c0c48ca46aaee8bdf09b5929d
usagi-mikan <makitakumiko228@gmail.com>
2021-05-28 13:16:43 +0900

    ビューファイル差替・コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "items#index"
+  resources :prototypes, only: [:index, :new]
 end
