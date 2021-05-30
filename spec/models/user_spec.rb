c3ac170664783017c9896c12d4608336f29c8b14
usagi-mikan <makitakumiko228@gmail.com>
2021-05-30 15:48:02 +0900

    モデルの単体テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,6 +70,36 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("First name reading can't be blank")
     end
 
+    it 'passwordが半角英数混合でないと登録できない(半角英語のみ)' do
+      @user.password = 'aaaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+    end
+
+    it "お名前のlast_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+      @user.last_name = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")
+    end
+
+    it "お名前のfirst_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+      @user.first_name = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+    end
+
+    it "お名前のlast_name_readingが全角（カタカナ）でないと登録できない" do
+      @user.last_name_reading = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name reading is invalid. Input full-width katakana characters.")
+    end
+
+    it "お名前のfirst_name_readingが全角（カタカナ）でないと登録できない" do
+      @user.first_name_reading = 'ｱｲｳｴｵ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name reading is invalid. Input full-width katakana characters.")
+    end
+
     it "birthdayが空では登録できない" do
       @user.birthday = ''
       @user.valid?
