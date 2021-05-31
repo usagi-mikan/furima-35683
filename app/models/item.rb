57083a212b12b140b95572188c7312f6a12cfabd
usagi-mikan <makitakumiko228@gmail.com>
2021-05-31 17:11:46 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :concept_category
+  belongs_to :concept_status
+  belongs_to :delivery_area
+  belongs_to :delivery_charge
+  belongs_to :delivery_day
 end
