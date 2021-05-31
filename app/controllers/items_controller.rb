4aaaa5a86e5c36e9f39a153f404e57dc5598d327
usagi-mikan <makitakumiko228@gmail.com>
2021-05-31 18:25:47 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :catch_copy, :concept_category, :concept_status, :delivery_charge, :delivery_area, :delivery_day).merge(user_id: current_user.id)
+  end
+  
 end
