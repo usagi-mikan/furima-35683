3831206296701b8f623f8ea43690e12e3ae45456
usagi-mikan <makitakumiko228@gmail.com>
2021-06-01 17:06:34 +0900

    販売手数料と販売利益の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', () => {
   console.log("OK");
-});
+
+// 金額を入力した数値をpriceInputという変数に格納する
+  const priceInput = document.getElementById("item-price");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      console.log(inputValue);
+
+      const addTaxDom = document.getElementById("add-tax-price")
+      addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+      console.log(addTaxDom);
+
+      const profitNumber = document.getElementById("profit")
+      profitNumber.innerHTML = (Math.floor(inputValue * 0.9));
+      console.log(profitNumber);
+
+    })
+
+})
