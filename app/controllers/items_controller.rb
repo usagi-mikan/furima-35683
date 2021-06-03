b4ae10eaebb015d3d54bbb43521c6e98b1d1fc62
usagi-mikan <makitakumiko228@gmail.com>
2021-06-03 14:44:43 +0900

    updateアクションの追加・エラーメッセージの表示・ログイン状態の画面遷移制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
@@ -27,9 +28,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(@item.id)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :title, :catch_copy, :concept_category_id, :concept_status_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
-  
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless  current_user.id == @item.user_id
+      redirect_to root_path(@item)
+    end
+  end
+
 end
