13c30fea82509a6a9e73edcbe1ed8fe9fee9c547
usagi-mikan <makitakumiko228@gmail.com>
2021-06-05 12:05:25 +0900

    ordersのルーティング・コントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,14 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def create
+    # @donation_address = DonationAddress.new(donation_params)
+    # if @donation_address.valid?
+    #   @donation_address.save
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
+  end
+end
---
+++ app/models/purchase_shipping_address.rb
@@ -0,0 +1,13 @@
+class PurchaseShippingAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :telehome, :price, :purchase_id, :user_id, :item_id
+
+  def save
+    # 購入情報を保存し、変数purchaseに代入する
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    # purchase_idには、変数purchaseのidと指定する
+    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, telephone: telephome, purchase_id: purchase.id)
+  end
+
+end
