53bdcf0bbb1e6ff1c710d3123c3471bd7d190915
usagi-mikan <makitakumiko228@gmail.com>
2021-06-06 15:25:19 +0900

    フォーム情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,24 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = PurchaseShippingAddress.new
   end
 
   def create
-    @order =PurchaseShippingAddress.new(item_params)
+    @item = Item.find(params[:item_id])
+    @order = PurchaseShippingAddress.new(order_params)
     if @order.valid?
       @order.save
       redirect_to root_path
     else
-      render :item_orders_path
+      render :index
     end
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :title, :delivery_charge_id, :price).merge(user_id: current_user.id)
+
+  def order_params
+    params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone ).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,6 +1,6 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :telehome, :price, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone, :price, :purchase_id, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
@@ -12,7 +12,7 @@ class PurchaseShippingAddress
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     # 住所を保存する
     # purchase_idには、変数purchaseのidと指定する
-    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, telephone: telephome, purchase_id: purchase.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone: telephone, purchase_id: purchase.id)
   end
 
 end
