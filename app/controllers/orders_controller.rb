694bcc3064ff3f2c6c336bb5e7d2ba05f7b5b608
usagi-mikan <makitakumiko228@gmail.com>
2021-06-07 20:35:25 +0900

    if文の再修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,10 +39,8 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user_id
-      if @item.purchase.present?
+    if current_user.id == @item.user_id || @item.purchase.present?
         redirect_to root_path(@item)
-      end
     end
   end
 
